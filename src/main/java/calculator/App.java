a0da28f25595d62c24d3db0dc40d8d0603b88cac
Robbie <robbie331@>
2025-07-10 21:10:40 +0900

    Level 1-2 사칙연산 기호를 char 자료형으로 입력받아 operatior 변수에 저장한다. * charAt(index) : index 번째에 위치한 값 반환

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -15,6 +15,10 @@ public class App {
                 System.out.print("두 번째 숫자를 입력하세요: ");
                 // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
                 int num2 = sc.nextInt();
+
+                System.out.print("사칙연산 기호를 입력하세요: ");
+                // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
+                char operator = sc.next().charAt(0);
             }
         }
 
