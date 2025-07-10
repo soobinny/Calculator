763d64a87d62d5b2782aef8892d10ab299360794
Robbie <robbie331@>
2025-07-10 21:34:14 +0900

    Level 1-4 while 반복문을 사용하여 반복의 종료를 알려주는 “exit” 문자열을 입력하기 전까지 무한으로 계산 진행

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -8,40 +8,50 @@ public class App {
             public static void main(String[] args) {
                 Scanner sc = new Scanner(System.in);
 
-                System.out.print("첫 번째 숫자를 입력하세요: ");
-                // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
-                int num1 = sc.nextInt();
+                while (true) {
+                    System.out.print("첫 번째 숫자를 입력하세요: ");
+                    // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+                    int num1 = sc.nextInt();
 
-                System.out.print("두 번째 숫자를 입력하세요: ");
-                // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
-                int num2 = sc.nextInt();
+                    System.out.print("두 번째 숫자를 입력하세요: ");
+                    // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+                    int num2 = sc.nextInt();
 
-                System.out.print("사칙연산 기호를 입력하세요: ");
-                // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
-                char operator = sc.next().charAt(0);
+                    System.out.print("사칙연산 기호를 입력하세요: ");
+                    // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
+                    char operator = sc.next().charAt(0);
 
-                int result = 0;
+                    int result = 0;
 
-                if(operator == '+') {
-                    result = num1 + num2;
-                    System.out.println("결과 : " + result);
-                } else if (operator == '-') {
-                    result = num1 - num2;
-                    System.out.println("결과 : " + result);
-                } else if (operator == '*') {
-                    result = num1 * num2;
-                    System.out.println("결과 : " + result);
-                } else{
-                    if(num2 == 0) {
-                        System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-                    }else{
-                        result = num1 / num2;
+                    if (operator == '+') {
+                        result = num1 + num2;
                         System.out.println("결과 : " + result);
-                    }
-                }
+                    } else if (operator == '-') {
+                        result = num1 - num2;
+                        System.out.println("결과 : " + result);
+                    } else if (operator == '*') {
+                        result = num1 * num2;
+                        System.out.println("결과 : " + result);
+                    } else {
+                        if (num2 == 0) {
+                            System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                        } else {
+                            result = num1 / num2;
+                            System.out.println("결과 : " + result);
+                        }
 
+                    }
+                    System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+                    String exit = sc.next();
+                    if (exit.equals("exit")) {
+                        break;
+
+                    }
+
+                }
             }
-        }
+    }
+
 
 
 
