0f1251af54cb80817941972bc0f9048adb3e57c7
Robbie <robbie331@>
2025-07-11 16:36:15 +0900

    Level 1-5 길이 10의 배열 생성 후 연산의 결과를 비어있는 곳에 저장

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -2,55 +2,64 @@ package calculator;
 
 import java.util.Scanner;
 
-
 public class App {
 
-            public static void main(String[] args) {
-                Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
 
-                while (true) {
-                    System.out.print("첫 번째 숫자를 입력하세요: ");
-                    // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
-                    int num1 = sc.nextInt();
+        int [] arr = new int [10];
+        int count = 0; // 배열 인덱스 카운트
 
-                    System.out.print("두 번째 숫자를 입력하세요: ");
-                    // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
-                    int num2 = sc.nextInt();
+        Scanner sc = new Scanner(System.in);
 
-                    System.out.print("사칙연산 기호를 입력하세요: ");
-                    // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
-                    char operator = sc.next().charAt(0);
+        while (true) {
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+            int num1 = sc.nextInt();
 
-                    int result = 0;
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+            int num2 = sc.nextInt();
 
-                    if (operator == '+') {
-                        result = num1 + num2;
-                        System.out.println("결과 : " + result);
-                    } else if (operator == '-') {
-                        result = num1 - num2;
-                        System.out.println("결과 : " + result);
-                    } else if (operator == '*') {
-                        result = num1 * num2;
-                        System.out.println("결과 : " + result);
-                    } else {
-                        if (num2 == 0) {
-                            System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-                        } else {
-                            result = num1 / num2;
-                            System.out.println("결과 : " + result);
-                        }
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
+            char operator = sc.next().charAt(0);
 
-                    }
-                    System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
-                    String exit = sc.next();
-                    if (exit.equals("exit")) {
-                        break;
+            int result = 0;
 
-                    }
-
+            if (operator == '+') {
+                result = num1 + num2;
+                System.out.println("결과 : " + result);
+            } else if (operator == '-') {
+                result = num1 - num2;
+                System.out.println("결과 : " + result);
+            } else if (operator == '*') {
+                result = num1 * num2;
+                System.out.println("결과 : " + result);
+            } else {
+                if (num2 == 0) {
+                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                } else {
+                    result = num1 / num2;
+                    System.out.println("결과 : " + result);
                 }
             }
+
+            if(count < arr.length) { // 배열 길이 (10)을 넘지 않아야 함
+                arr[count] = result;
+                count++; // 인덱스 카운트 증가
+            }
+
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            String exit = sc.next();
+            if (exit.equals("exit")) {
+                break;
+            }
+        }
     }
+}
+
+
+
 
 
 
