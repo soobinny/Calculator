f2e3c106253ac2cf2d364a0767ab0b48b4c0ad62
Robbie <robbie331@>
2025-07-14 16:43:03 +0900

    Level 2-2 ~ 2-6 1. App 클래스에 Calculator 인스턴스 추가 2. Calculator 클래스에 Getter, Setter 추가 3. Calculator 클래스에 removeResult,inquiryResult 구현 4. Calculator 클래스 생성자 구현

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -6,11 +6,7 @@ public class App {
 
     public static void main(String[] args) {
 
-        List<Integer> resultArr = new ArrayList<>(); // List Framework 사용
-
-//        int [] arr = new int [10];
-//        int count = 0; // 배열 인덱스 카운트
-
+        Calculator calculator = new Calculator(); // 2-2) Calculator 인스턴스 생성
         Scanner sc = new Scanner(System.in);
 
         while (true) {
@@ -26,48 +22,18 @@ public class App {
             // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
             char operator = sc.next().charAt(0);
 
-            int result = 0;
+            int result = calculator.calculate(num1, num2, operator);
 
-            if (operator == '+') {
-                result = num1 + num2;
-                System.out.println("결과 : " + result);
-            } else if (operator == '-') {
-                result = num1 - num2;
-                System.out.println("결과 : " + result);
-            } else if (operator == '*') {
-                result = num1 * num2;
-                System.out.println("결과 : " + result);
-            } else {
-                if (num2 == 0) {
-                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-                } else {
-                    result = num1 / num2;
-                    System.out.println("결과 : " + result);
-                }
-            }
-
-//            if(count < arr.length) { // 배열 길이 (10)을 넘지 않아야 함
-//                arr[count] = result;
-//                count++; // 인덱스 카운트 증가
-//            }else{
-//                for(int i = 1; i < arr.length; i++){
-//                    arr[i-1] = arr[i];
-//                }arr[9] =  result;
-//            }
-            resultArr.add(result);
             System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
             String remove = sc.next();
-            if (remove.equals("remove")) { // remove 입력 시 가장 먼저 저장된 결과 삭제
-                resultArr.remove(0);
+            if (remove.equals("remove")) {
+                calculator.removeResult();
             }
 
             System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
             String inquiry = sc.next();
             if (inquiry.equals("inquiry")) {
-                for(int i : resultArr){ // inquiry 입력 시 for - each문을 이용하여 resultArr 출력
-                    System.out.print(i + " ");
-                }
-                System.out.println();
+                calculator.inquiryResult();
             }
 
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -3,7 +3,12 @@ package calculator;
 import java.util.*;
 
 public class Calculator {
-    private List<Integer> resultArr = new ArrayList<>();
+
+    private List<Integer> resultArr; // 2-6) List 컬렉션 변수 선언
+
+    public Calculator() {
+        this. resultArr = new ArrayList<>(); // 2-6) ArrayList로 구현
+    }
 
     public int calculate(int num1, int num2, char operator) {
 
@@ -33,8 +38,26 @@ public class Calculator {
                 throw new IllegalArgumentException("잘못된 연산자입니다.");
 
         }
-
         resultArr.add(result);
         return result;
     }
-}
+
+    public List<Integer> getResultArr() { // 2-3 ) 캡슐화 - Getter 메서드 구현
+        return resultArr;
+    }
+
+    public void setResultArr(List<Integer> resultArr) { //// 2-3 ) 캡슐화 - Setter 메서드 구현
+        this.resultArr = resultArr;
+    }
+
+    public void removeResult(){ // 2-4 ) 첫 번째 인자 삭제하는 메서드 구현
+        resultArr.remove(0);
+    }
+
+    public void inquiryResult(){ // 2-5 ) resultArr 조회하는 메서드 구현
+            for(int i : resultArr){ // inquiry 입력 시 for - each문을 이용하여 resultArr 출력
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
