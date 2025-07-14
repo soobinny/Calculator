8ea01138e96a00279b242e11a2eafeb3119aeddd
Robbie <robbie331@>
2025-07-14 09:14:55 +0900

    Level 2-1 1. 사칙연산을 수행하는 Calculator 클래스 생성 2. 나눗셈에서 분모에 0이 들어오거나 연산자 기호가 잘 못 들어온 경우 적합한 Exception 클래스를 생성하여 throw

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,5 +1,40 @@
 package calculator;
 
-public class Calculator {
+import java.util.*;
 
+public class Calculator {
+    private List<Integer> resultArr = new ArrayList<>();
+
+    public int calculate(int num1, int num2, char operator) {
+
+        int result = 0;
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+
+            case '-':
+                result = num1 - num2;
+                break;
+
+            case '*':
+                result = num1 * num2;
+                break;
+
+            case '/':
+                if (num2 == 0) {
+                    throw new ArithmeticException("0으로 나눌 수 없습니다.");
+                }
+                result = num1 / num2;
+                break;
+
+            default:
+                throw new IllegalArgumentException("잘못된 연산자입니다.");
+
+        }
+
+        resultArr.add(result);
+        return result;
+    }
 }
