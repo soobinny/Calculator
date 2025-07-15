79dbe2f201dd5dcbd69a07a6cfde249b655640ed
Robbie <robbie331@>
2025-07-15 14:40:04 +0900

    Level 2-7 원의 넓이 메서드 구현 - 원의 반지름 입력받기 - 넓이 저장하는 컬렉션 생성 - 생성자 - getter, setter - 결과 리스트 조회

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -9,45 +9,66 @@ public class App {
         Calculator calculator = new Calculator(); // 2-2) Calculator 인스턴스 생성
         Scanner sc = new Scanner(System.in);
 
-        while (true) {
-            System.out.print("첫 번째 숫자를 입력하세요: ");
-            // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
-            int num1 = sc.nextInt();
+        while(true){
 
-            System.out.print("두 번째 숫자를 입력하세요: ");
-            // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
-            int num2 = sc.nextInt();
+            //원의 넓이 / 사칙연산 Scanner로 입력받기
+            System.out.print("원의 넓이 구하기 (Circle) 혹은 사칙 연산 (Calculator) 중 선택하여 입력하세요: ");
+            String input = sc.nextLine();
 
-            System.out.print("사칙연산 기호를 입력하세요: ");
-            // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
-            char operator = sc.next().charAt(0);
 
-            int result = calculator.calculate(num1, num2, operator);
+            if (input.equals("Circle")) {
+                System.out.print("원의 반지름을 입력하세요 : ");
+                double radius = sc.nextDouble();
+                sc.nextLine();
+                calculator.calculateCircleArea(radius);
+                calculator.inquiryArea();
 
-            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
-            String remove = sc.next();
-            if (remove.equals("remove")) {
-                calculator.removeResult();
-            }
+            }else if (input.equals("Calculator")) {
+                while (true) {
+                    System.out.print("첫 번째 숫자를 입력하세요: ");
+                    // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+                    int num1 = sc.nextInt();
 
-            System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
-            String inquiry = sc.next();
-            if (inquiry.equals("inquiry")) {
-                calculator.inquiryResult();
-            }
+                    System.out.print("두 번째 숫자를 입력하세요: ");
+                    // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+                    int num2 = sc.nextInt();
 
-            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
-            String exit = sc.next();
-            if (exit.equals("exit")) {
-                break;
-            }
+                    System.out.print("사칙연산 기호를 입력하세요: ");
+                    // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
+                    char operator = sc.next().charAt(0);
+
+                    int result = calculator.calculate(num1, num2, operator);
+
+                    System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+                    String remove = sc.next();
+                    if (remove.equals("remove")) {
+                        calculator.removeResult();
+                    }
+
+                    System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+                    String inquiry = sc.next();
+                    if (inquiry.equals("inquiry")) {
+                        calculator.inquiryResult();
+                    }
+
+                    System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+                    String exit = sc.next();
+                    if (exit.equals("exit")) {
+                        break;
+                    }
+
         }
+
+
+            }
+
+        }
+
+
+
+
     }
 }
-
-
-
-
 
 
 
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -4,10 +4,18 @@ import java.util.*;
 
 public class Calculator {
 
-    private List<Integer> resultArr; // 2-6) List 컬렉션 변수 선언
+    private List<Integer> calcArr; // 2-6) List 컬렉션 변수 선언
+    private List<Double> areaArr;
+
+    private static final double PI = 3.14;
+    /*
+    * static : 모든 인스턴스가 공유하는 상수 -> 메모리 절약
+    * final : 불변 값 -> 원주율은 불변값!
+    * */
 
     public Calculator() {
-        this. resultArr = new ArrayList<>(); // 2-6) ArrayList로 구현
+        this.calcArr = new ArrayList<>(); // 2-6) ArrayList로 구현
+        this.areaArr = new ArrayList<>();
     }
 
     public int calculate(int num1, int num2, char operator) {
@@ -38,26 +46,52 @@ public class Calculator {
                 throw new IllegalArgumentException("잘못된 연산자입니다.");
 
         }
-        resultArr.add(result);
+        calcArr.add(result);
         return result;
     }
 
-    public List<Integer> getResultArr() { // 2-3 ) 캡슐화 - Getter 메서드 구현
-        return resultArr;
+    public void calculateCircleArea(double radius){ // 원의 반지름을 입력받아 원의 넓이 출력
+        double circleResult = PI * radius * radius;
+        areaArr.add(circleResult);
+        System.out.printf("반지름이 %.2f인 원의 넓이: %.2f\n", radius, circleResult);
     }
 
-    public void setResultArr(List<Integer> resultArr) { //// 2-3 ) 캡슐화 - Setter 메서드 구현
-        this.resultArr = resultArr;
+    public List<Integer> getResultArr() { // 2-3 ) 캡슐화 - Getter 메서드 구현
+        return calcArr;
+    }
+
+    public void setResultArr(List<Integer> calcArr) { // 2-3 ) 캡슐화 - Setter 메서드 구현
+        this.calcArr = calcArr;
+    }
+
+    public List<Double> getAreaArr() { //2-7 ) 캡슐화 - Getter 메서드 구현
+        return areaArr;
+    }
+
+    public void setAreaArr(List<Double> areaArr) { // 2-7 ) 캡슐화 - Setter 메서드 구현
+        this.areaArr = areaArr;
     }
 
     public void removeResult(){ // 2-4 ) 첫 번째 인자 삭제하는 메서드 구현
-        resultArr.remove(0);
+        calcArr.remove(0);
     }
 
     public void inquiryResult(){ // 2-5 ) resultArr 조회하는 메서드 구현
-            for(int i : resultArr){ // inquiry 입력 시 for - each문을 이용하여 resultArr 출력
+            for(int i : calcArr){ // inquiry 입력 시 for - each문을 이용하여 resultArr 출력
                 System.out.print(i + " ");
             }
             System.out.println();
         }
+
+        public void inquiryArea(){ // 원의 넓이 리스트 조회
+        System.out.println();
+        System.out.println("[현재 저장된 원의 넓이 리스트]");
+        for(Double i : areaArr){
+            System.out.printf("%.2f", i);
+            System.out.println();
+        }
+        System.out.println();
+        }
     }
+
+
